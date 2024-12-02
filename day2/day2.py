31d71ab426d9e70e147d75d9dec1181cba7c1d10
IsaacIngram <me@isaacingram.dev>
2024-12-02 09:53:51 -0500

    Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -10,30 +10,37 @@
 
 INPUT_PATH = "input.txt"
 
+
+def are_levels_safe(report) -> bool:
+    previous = report[0]
+    passed = True
+    for level in report[1:]:
+        if not (1 <= abs(level - previous) <= 3):
+            return False
+        previous = level
+    return True
+
+
 def main():
 
-    all_reports = list()
     with open(INPUT_PATH, 'r') as input_file:
-        for line in input_file.readlines():
-            split_line = line.split()
-            report = list()
-            for i in split_line:
-                report.append(int(i))
-            all_reports.append(report)
+        all_reports = [[int(i) for i in line.split()] for line in input_file.readlines()]
 
     part_1 = 0
+    part_2 = 0
     for report in all_reports:
-        if sorted(report) == report or sorted(report, reverse=True) == report:
-            previous = report[0]
-            passed = True
-            for level in report[1:]:
-                if not (1 <= abs(level - previous) <= 3):
-                    passed = False
-                previous = level
-            if passed:
+        if (sorted(report) == report or sorted(report, reverse=True) == report) and are_levels_safe(report):
                 part_1 += 1
+        else:
+            for i in range(len(report)):
+                new_report = report.copy()
+                new_report.pop(i)
+                if (sorted(new_report) == new_report or sorted(new_report, reverse=True) == new_report) and are_levels_safe(new_report):
+                    part_2 += 1
+                    break
 
     print(part_1)
+    print(part_2 + part_1)
 
 if __name__ == '__main__':
     main()
