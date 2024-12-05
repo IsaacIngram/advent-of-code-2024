7240d80003106a118816159d940c4b031245d335
IsaacIngram <me@isaacingram.dev>
2024-12-05 10:45:27 -0500

    Day 5 part 2

--- day5/day5.py
+++ day5/day5.py
@@ -34,6 +34,7 @@ def main():
                 updates_to_perform.append([int(x) for x in line.split(",")])
 
     part1 = 0
+    part2 = 0
     for page_update in updates_to_perform:
         is_ordered = True
         for i in range(len(page_update)):
@@ -42,8 +43,19 @@ def main():
                     is_ordered = False
         if is_ordered:
             part1 += page_update[len(page_update) // 2]
-    print(part1)
+        else:
+            # Need to reorder
+            for i in range(len(page_update) - 1, 0, -1):
+                for j in range(i - 1, -1, -1):
+                    current_val = page_update[i]
+                    compared_val = page_update[j]
+                    if compared_val in adjacency_list[current_val]:
+                        # Swap
+                        page_update[i], page_update[j] = page_update[j], page_update[i]
+            part2 += page_update[len(page_update) // 2]
 
+    print(part1)
+    print(part2)
 
 if __name__ == '__main__':
     main()
