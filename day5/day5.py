f9117a299f160ba6c1ca46d77c5ae926a003af0e
IsaacIngram <me@isaacingram.dev>
2024-12-05 00:29:23 -0500

    Day 5 part 1

---
+++ day5/day5.py
@@ -0,0 +1,49 @@
+###############################################################################
+#
+# File: day5.py
+#
+# Author: Isaac Ingram
+#
+# Purpose:
+#
+###############################################################################
+
+INPUT_PATH = 'input.txt'
+
+def main():
+
+    adjacency_list = dict()
+    read_updates = False
+    updates_to_perform = list()
+    with open(INPUT_PATH, 'r') as input_file:
+        for line in input_file.readlines():
+            # Switch to reading updates when empty line is encountered
+            if line == "\n":
+                read_updates = True
+                continue
+            if not read_updates:
+                # Reading in adjacency list
+                before_s, after_s = line.split("|")
+                if int(before_s) in adjacency_list:
+                    adjacency_list[int(before_s)].add(int(after_s))
+                else:
+                    adjacency_list[int(before_s)] = set()
+                    adjacency_list[int(before_s)].add(int(after_s))
+            else:
+                # Reading in updates
+                updates_to_perform.append([int(x) for x in line.split(",")])
+
+    part1 = 0
+    for page_update in updates_to_perform:
+        is_ordered = True
+        for i in range(len(page_update)):
+            for j in range(i + 1, len(page_update)):
+                if page_update[j] not in adjacency_list[page_update[i]]:
+                    is_ordered = False
+        if is_ordered:
+            part1 += page_update[len(page_update) // 2]
+    print(part1)
+
+
+if __name__ == '__main__':
+    main()
